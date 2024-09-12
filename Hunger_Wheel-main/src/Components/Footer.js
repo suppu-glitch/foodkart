d6c1087398e0a100985ffa4b65b5f3e93e693e93
suppu-glitch <supriyagosabal98@gmail.com>
2024-09-12 18:09:53 +0530

    Add files via upload
    uploaded the react project

---
+++ Hunger_Wheel-main/src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <h4 className="p-10 text-center">
+      This site is developed by{" "}
+      <a
+        className="text-orange-400"
+        href="https://github.com/arun22ayi/Hunger_Wheel"
+        target="_blank"
+      >
+        Arun Ireshanavar
+      </a>
+    </h4>
+  );
+};
+
+export default Footer;
